d33812e0d163b64479e119f10baf201bd3d02e74
Victor Huatuco <victorhuatuco@gmail.com>
2021-03-15 16:31:24 -0500

    Mejora de rendimiendo con programacion dinamica de fibonacci

---
+++ programacion_dinamica.py
@@ -0,0 +1,21 @@
+import sys
+
+def fibonacci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1
+    return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+
+def fibonacci_dinamico(n, memoria = {}):
+    if n == 0 or n == 1:
+        return 1
+    try:
+        return memoria [n]
+    except KeyError:
+        resultado = fibonacci_dinamico(n-1, memoria) + fibonacci_dinamico(n-2, memoria)
+        memoria[n] = resultado
+        return resultado
+if __name__ == '__main__':
+    sys.setrecursionlimit(2000)
+    n = int(input('Escoge un numero'))
+    resultado = fibonacci_dinamico(n)
+    print(resultado)
